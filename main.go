d199a9fb746d38e6a06ba91bc7b9e0dc0beca725
Diogo Recharte <diogo.recharte@percona.com>
2023-01-11 09:57:05 +0000

    PMM-11304 add DBEngine controller

--- main.go
+++ main.go
@@ -107,6 +107,13 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "Database")
 		os.Exit(1)
 	}
+	if err = (&controllers.DatabaseEngineReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "DatabaseEngine")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
