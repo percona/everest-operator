9896aa1495ff6ae68569f441311f2ef068338bc9
Andrew Minkin <andrew.minkin@percona.com>
2023-08-14 18:00:05 +0600

    EVEREST-256 Added first version of admission webhook

--- main.go
+++ main.go
@@ -128,6 +128,10 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "DatabaseClusterBackup")
 		os.Exit(1)
 	}
+	if err = (&everestv1alpha1.DatabaseCluster{}).SetupWebhookWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create webhook", "webhook", "DatabaseCluster")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
