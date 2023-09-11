a7fcf08b63daeee03f36fe395aa48a722dc76090
Andrew Minkin <andrew.minkin@percona.com>
2023-09-11 13:39:33 +0600

    EVEREST-356 Fixed linter

--- controllers/databaseclusterrestore_controller.go
+++ controllers/databaseclusterrestore_controller.go
@@ -65,8 +65,6 @@ type DatabaseClusterRestoreReconciler struct {
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
-//
-//nolint:gocognit
 func (r *DatabaseClusterRestoreReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 	logger := log.FromContext(ctx)
 	logger.Info("reconciling", "request", req)
