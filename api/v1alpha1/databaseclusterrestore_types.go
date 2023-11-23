bd0ecdfeb9ccbb14632b5dfc4224d8f323f4d754
Oksana Grishchenko <oksana.grishchenko@percona.com>
2023-11-23 11:20:33 +0200

    EVEREST-518 pitr type

--- api/v1alpha1/databaseclusterrestore_types.go
+++ api/v1alpha1/databaseclusterrestore_types.go
@@ -25,6 +25,14 @@ import (
 // RestoreState represents state of restoration.
 type RestoreState string
 
+// PITRType represents type of Point-in-time recovery.
+type PITRType string
+
+const (
+	PITRTypeDate   PITRType = "date"
+	PITRTypeLatest PITRType = "latest"
+)
+
 // DatabaseClusterRestoreSpec defines the desired state of DatabaseClusterRestore.
 type DatabaseClusterRestoreSpec struct {
 	// DBClusterName defines the cluster name to restore.
@@ -43,9 +51,9 @@ type DatabaseClusterRestoreStatus struct {
 // PITR represents a specification to configure point in time recovery for a database backup/restore.
 type PITR struct {
 	// Type is the type of recovery. pxc: full list; psmdb, pg: date,latest
-	// +kubebuilder:validation:Enum:=date;latest;transaction;skip
+	// +kubebuilder:validation:Enum:=date;latest
 	// +kubebuilder:default:=date
-	Type string `json:"type,omitempty"`
+	Type PITRType `json:"type,omitempty"`
 	// Date is the date to recover to
 	Date *RestoreDate `json:"date,omitempty"`
 }
