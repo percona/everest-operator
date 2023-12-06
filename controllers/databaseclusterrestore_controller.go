efda38b90552c14af71f4f3e682572f94cd486aa
Oksana Grishchenko <oksana.grishchenko@percona.com>
2023-12-06 17:50:00 +0200

    EVEREST-603 restore

--- controllers/databaseclusterrestore_controller.go
+++ controllers/databaseclusterrestore_controller.go
@@ -341,6 +341,17 @@ func (r *DatabaseClusterRestoreReconciler) restorePXC(
 				return fmt.Errorf("unsupported backup storage type %s for %s", backupStorage.Spec.Type, backupStorage.Name)
 			}
 		}
+
+		if restore.Spec.DataSource.PITR != nil {
+			pxcCR.Spec.PITR = &pxcv1.PITR{
+				BackupSource: &pxcv1.PXCBackupStatus{
+					StorageName: pitrStorageName(db.Spec.Backup.PITR.BackupStorageName),
+				},
+				Type: string(restore.Spec.DataSource.PITR.Type),
+				Date: restore.Spec.DataSource.PITR.Date.Format(everestv1alpha1.DateFormat),
+			}
+		}
+
 		return nil
 	})
 	if err != nil {
