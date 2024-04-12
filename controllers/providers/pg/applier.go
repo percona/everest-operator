18ba4abbefc7d821d2bab945d6c464fa646c9fd2
Mayank Shah <mayank.shah@percona.com>
2024-04-12 17:50:58 +0530

    handle postgres differently
    Signed-off-by: Mayank Shah <mayank.shah@percona.com>

--- controllers/providers/pg/applier.go
+++ controllers/providers/pg/applier.go
@@ -74,9 +74,22 @@ func (p *applier) Engine() error {
 		database.Spec.Engine.Version = engine.BestEngineVersion()
 	}
 
-	// Update CRVersion, if specified.
+	// Set a CRVersion.
+	observedCRVersion := p.DB.Status.CRVersion
 	desiredCR := pointer.Get(p.DB.Spec.Engine.CRVersion)
-	if desiredCR != "" {
+	if observedCRVersion == "" && desiredCR == "" {
+		// During the initial installation, a CRVersion may not be provided.
+		// So we will use the operator version.
+		v, err := common.GetOperatorVersion(p.ctx, p.C, types.NamespacedName{
+			Name:      common.PGDeploymentName,
+			Namespace: p.DB.GetNamespace(),
+		})
+		if err != nil {
+			return err
+		}
+		pg.Spec.CRVersion = v.ToCRVersion()
+	} else if desiredCR != "" {
+		// Otherwise, we always use the one provided.
 		pg.Spec.CRVersion = desiredCR
 	}
 
