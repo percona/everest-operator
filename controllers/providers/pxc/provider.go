e34b847d8d4c0aedecdcf0ae14ad28d7aa47bb5b
Mayank Shah <mayank.shah@percona.com>
2024-04-19 21:00:30 +0530

    EVEREST-590 | Fix regression in PXC restores (#372)
    Signed-off-by: Mayank Shah <mayank.shah@percona.com>

--- controllers/providers/pxc/provider.go
+++ controllers/providers/pxc/provider.go
@@ -61,6 +61,8 @@ func New(
 		pxc)
 	if err != nil && !k8serrors.IsNotFound(err) {
 		return nil, err
+	} else if k8serrors.IsNotFound(err) {
+		pxc.Spec = defaultSpec()
 	}
 
 	// Add necessary finalizers.
@@ -78,8 +80,6 @@ func New(
 		return nil, err
 	}
 	opts.DBEngine = dbEngine
-
-	pxc.Spec = defaultSpec()
 
 	p := &Provider{
 		PerconaXtraDBCluster: pxc,
