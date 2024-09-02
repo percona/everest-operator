b6b073b01090d8da0384f0739b313cc31d2b2b9a
Oksana Grishchenko <oksana.grishchenko@percona.com>
2024-09-02 17:22:06 +0200

    format

--- controllers/providers/psmdb/applier.go
+++ controllers/providers/psmdb/applier.go
@@ -190,7 +190,7 @@ func (p *applier) Proxy() error {
 
 	// otherwise configure psmdb.Spec.Sharding.Mongos according to the db proxy settings
 	if psmdb.Spec.Sharding.Mongos == nil {
-		var size = database.Spec.Engine.Replicas
+		size := database.Spec.Engine.Replicas
 		if database.Spec.Proxy.Replicas != nil {
 			size = *database.Spec.Proxy.Replicas
 		}
