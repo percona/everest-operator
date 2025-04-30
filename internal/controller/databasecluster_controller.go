c0f883f209bf8c79972881d43cb6e7497dd34086
Maxim Kondratenko <maxim.kondratenko@percona.com>
2025-04-30 18:02:16 +0300

    EVEREST-1651 Fix setting BackupStorageName into DatabaseCluster.labels (#681)

--- internal/controller/databasecluster_controller.go
+++ internal/controller/databasecluster_controller.go
@@ -403,10 +403,31 @@ func (r *DatabaseClusterReconciler) reconcileLabels(
 	updated := make(map[string]string, len(current))
 	maps.Copy(updated, current)
 
-	updated[databaseClusterNameLabel] = database.Name
+	updated[databaseClusterNameLabel] = database.GetName()
 	if database.Spec.DataSource != nil {
-		updated[fmt.Sprintf(backupStorageNameLabelTmpl, database.Spec.DataSource.DBClusterBackupName)] = backupStorageLabelValue
+		if database.Spec.DataSource.DBClusterBackupName != "" {
+			// need to obtain backupStorageName by .spec.dataSource.dbClusterBackupName.dbClusterBackupName
+			dbBackup := &everestv1alpha1.DatabaseClusterBackup{}
+			err := r.Get(ctx, types.NamespacedName{
+				Name:      database.Spec.DataSource.DBClusterBackupName,
+				Namespace: database.GetNamespace(),
+			}, dbBackup)
+			if err != nil {
+				return errors.Join(err, fmt.Errorf("could not get DB backup '%s' to obtain backup storage name for DB cluster='%s' in namespace='%s'",
+					database.Spec.DataSource.DBClusterBackupName,
+					database.GetName(),
+					database.GetNamespace()))
+			}
+			updated[fmt.Sprintf(backupStorageNameLabelTmpl, dbBackup.Spec.BackupStorageName)] = backupStorageLabelValue
+		}
+
+		// .spec.dataSource.backupSource.backupStorageName can be set via API directly.
+		// UI doesn't set it right now.
+		if database.Spec.DataSource.BackupSource != nil && database.Spec.DataSource.BackupSource.BackupStorageName != "" {
+			updated[fmt.Sprintf(backupStorageNameLabelTmpl, database.Spec.DataSource.BackupSource.BackupStorageName)] = backupStorageLabelValue
+		}
 	}
+
 	if bkpStorageName := pointer.Get(database.Spec.Backup.PITR.BackupStorageName); bkpStorageName != "" {
 		updated[fmt.Sprintf(backupStorageNameLabelTmpl, bkpStorageName)] = backupStorageLabelValue
 	}
