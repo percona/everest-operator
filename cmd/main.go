7af723c35a73c73311377b6f471709ad44f2e216
Mayank Shah <mayank.shah@percona.com>
2025-05-02 12:07:52 +0530

    wire up
    Signed-off-by: Mayank Shah <mayank.shah@percona.com>

--- cmd/main.go
+++ cmd/main.go
@@ -284,6 +284,13 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "MonitoringConfig")
 		os.Exit(1)
 	}
+	if err = (&controllers.DataImportJobReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "DataImportJob")
+		os.Exit(1)
+	}
 	// +kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
