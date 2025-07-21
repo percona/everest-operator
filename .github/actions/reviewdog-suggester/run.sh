5c38f2fe487063403bb567dd1d665eceeff00e72
Maxim Kondratenko <max.kondr@gmail.com>
2025-07-21 16:44:32 +0300

    test

---
+++ .github/actions/reviewdog-suggester/run.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+set -e
+
+export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
+
+TMPFILE=$(mktemp)
+git diff >"${TMPFILE}"
+
+git stash -u
+
+# shellcheck disable=SC2086
+go tool reviewdog \
+  -name="${INPUT_TOOL_NAME:-reviewdog-suggester}" \
+  -f=diff \
+  -f.diff.strip=1 \
+  -reporter="github-pr-review" \
+  -filter-mode="${INPUT_FILTER_MODE}" \
+  -fail-level="${INPUT_FAIL_LEVEL}" \
+  -level="${INPUT_LEVEL}" \
+  ${INPUT_REVIEWDOG_FLAGS} <"${TMPFILE}" # INPUT_REVIEWDOG_FLAGS is intentionally split to pass multiple flags
+
+EXIT_CODE=$?
+
+if [ "${INPUT_CLEANUP}" = "true" ]; then
+  git stash drop || true
+else
+  git stash pop || true
+fi
+
+exit "${EXIT_CODE}"
