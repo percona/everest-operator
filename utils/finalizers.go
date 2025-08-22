abbd2cbad0f6d9cd11a7f1532ed94ca22baa1264
Oksana Grishchenko <oksana.grishchenko@percona.com>
2025-08-22 14:33:24 +0500

    static checks

--- utils/finalizers.go
+++ utils/finalizers.go
@@ -1,5 +1,5 @@
 // everest-operator
-// Copyright (C) 2025 Percona LLC
+// Copyright (C) 2022 Percona LLC
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,6 +12,9 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
+// Package utils ...
+//
 
 package utils
 
--- utils/validators_test.go
+++ utils/validators_test.go
@@ -1,5 +1,5 @@
 // everest-operator
-// Copyright (C) 2025 Percona LLC
+// Copyright (C) 2022 Percona LLC
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -13,7 +13,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Package utils contains common utils
 package utils
 
 import (
