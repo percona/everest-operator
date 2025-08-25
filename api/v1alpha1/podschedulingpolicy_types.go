35877afc5e3b81d64fdd8fdf71089a4bcd307ab9
Mayank Shah <mayank.shah@percona.com>
2025-08-25 12:19:56 +0530

    EVEREST-2224 | remove selectableFields from PSP CRD (#862)
    Signed-off-by: Mayank Shah <mayank.shah@percona.com>

--- api/v1alpha1/podschedulingpolicy_types.go
+++ api/v1alpha1/podschedulingpolicy_types.go
@@ -78,7 +78,6 @@ type PodSchedulingPolicyStatus struct {
 // +kubebuilder:object:root=true
 // +kubebuilder:subresource:status
 // +kubebuilder:resource:scope=Cluster,path=podschedulingpolicies,shortName=psp
-// +kubebuilder:selectablefield:JSONPath=".spec.engineType"
 // +kubebuilder:printcolumn:name="Engine",type="string",JSONPath=".spec.engineType",description="DB engine type the policy can be applied to"
 // +kubebuilder:printcolumn:name="InUse",type="string",JSONPath=".status.inUse",description="Indicates if the policy is used by any DB cluster"
 
